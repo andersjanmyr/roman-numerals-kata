138f1c6008520a0a98547eaa4a7f9f3981b73f8f
Anders Janmyr <anders@janmyr.com>
2013-03-13 14:49:55 +0100

    Added documentation links

--- phpunit/src/roman-numerals.php
+++ phpunit/src/roman-numerals.php
@@ -1,4 +1,9 @@
 <?php
+
+/*
+ * Documentation
+ * http://php.net/manual/en/index.php
+ */
 function decimalToRoman($number)
 {
    return $number;
--- phpunit/test/roman-numerals-test.php
+++ phpunit/test/roman-numerals-test.php
@@ -2,6 +2,10 @@
 require 'vendor/autoload.php';
 require_once(dirname(__FILE__)) . '/../src/roman-numerals.php';
 
+/*
+ * Documentation
+ * http://www.phpunit.de/manual/current/en/phpunit-book.html
+ */
 class RomanNumeralsTest extends PHPUnit_Framework_TestCase
 {
     public function test1ReturnsI()
