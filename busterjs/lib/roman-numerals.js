99c07fabe321cffd9fe8caad5c16eb4af478a066
Hugo Josefson <hugo.josefson@jayway.com>
2013-03-20 11:02:07 +0100

    Support for 0.

--- busterjs/lib/roman-numerals.js
+++ busterjs/lib/roman-numerals.js
@@ -1,4 +1,10 @@
 function romanToDecimal(number) {
+
+    if (number === "") {
+        return 0;
+    }
+
+    throw new Error("Unsupported Roman number: " + number);
 }
 
 module.exports = romanToDecimal;
