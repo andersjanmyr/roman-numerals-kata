2fd54af8dd548528b2f38e9c1ad3d59dd1dc0176
Anders Janmyr <anders@janmyr.com>
2013-03-20 11:03:59 +0100

    Added support for one.

--- busterjs/lib/roman-numerals.js
+++ busterjs/lib/roman-numerals.js
@@ -1,8 +1,9 @@
 function romanToDecimal(number) {
 
-    if (number === "") {
+    if (number === "")
         return 0;
-    }
+    if (number === "I")
+        return 1;
 
     throw new Error("Unsupported Roman number: " + number);
 }
