ca5685cbdaf14c75beafadaf1133947a10322d1a
Anders Janmyr <anders@janmyr.com>
2013-03-20 08:54:45 +0100

    Added support for 10

--- phpunit/src/roman-numerals.php
+++ phpunit/src/roman-numerals.php
@@ -6,7 +6,7 @@
  */
 function decimalToRoman($number)
 {
-  $digits = array('', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX');
+  $digits = array('', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X');
   if (count($digits) > $number)
     return $digits[$number];
 
