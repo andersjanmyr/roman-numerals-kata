3740a3bbe0840bcf8727148597a6329f3fc917ce
Hugo Josefson <hugo.josefson@jayway.com>
2013-03-20 09:34:13 +0100

    Support to 19, inclusive.

--- phpunit/src/roman-numerals.php
+++ phpunit/src/roman-numerals.php
@@ -6,10 +6,38 @@
  */
 function decimalToRoman($number)
 {
-  $digits = array('', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X');
-  if (count($digits) > $number)
-    return $digits[$number];
+    $numberAsString = strval($number);
 
-  throw new Exception('Unsupported Number: '.$number);
+    $roman = "";
+    $count = strlen($numberAsString);
+//    echo("count is $count.\n");
+    for ($posR = 0; $posR < $count; $posR++) {
+        $posL = $count - 1 - $posR;
+//        echo ("$numberAsString: $numberAsString{$posL} at $posL from left $posR from right.\n");
+        $romanDigit = digitToRoman($numberAsString{$posL}, $posR);
+        $roman = $romanDigit.$roman;
+    }
+
+    return $roman;
+
+}
+
+function digitToRoman($digit, $positionFromRight)
+{
+    if ($positionFromRight == 0) {
+        $digits0 = array('', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX');
+        if (count($digits0) > $digit)
+            return $digits0[$digit];
+    }
+
+    if ($positionFromRight == 1) {
+        $digits1 = array('', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC');
+        if (count($digits1) > $digit)
+            return $digits1[$digit];
+    }
+
+    throw new Exception('Unsupported Digit: ' . $digit);
+
+
 }
 ?>
