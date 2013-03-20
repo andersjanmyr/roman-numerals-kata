70f85a6220731a63bb96370c3df7fec948cc9d7f
Anders Janmyr <anders@janmyr.com>
2013-03-20 10:30:34 +0100

    Added more logic to generate the table

--- phpunit/src/roman-numerals.php
+++ phpunit/src/roman-numerals.php
@@ -21,17 +21,38 @@ function decimalToRoman($number)
 
 function digitToRoman($digit, $positionFromRight)
 {
-    $digits = array(
-        array('', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'),
-        array('', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'),
-        array('', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'),
-        array('', 'M', 'MM', 'MMM', 'MV̄', 'V̄', 'V̄M', 'V̄MM', 'V̄MMM', 'MX̄')
-      );
-
-    if ($positionFromRight + 1 > count($digits))
+    $powersOfTen = powersOfTen();
+    if ($positionFromRight + 1 > count($powersOfTen))
       throw new Exception("Unsupported Digit at position $positionFromRight: " . $digit);
 
-    $arr = $digits[$positionFromRight];
+    $arr = $powersOfTen[$positionFromRight];
     return $arr[$digit];
+}
+
+
+function powersOfTen()
+{
+  $romanLiterals = array('I', 'V', 'X', 'L', 'C', 'D', 'M', 'V̄', 'X̄');
+
+  $table = array();
+  for ($pt = 0; $pt < 4; $pt++) {
+    $first = $pt*2;
+    $one = $romanLiterals[$first];
+    $five = $romanLiterals[$first+1];
+    $ten = $romanLiterals[$first+2];
+    array_push($table, array(
+      "",
+      "$one",
+      "$one$one",
+      "$one$one$one",
+      "$one$five",
+      "$five",
+      "$five$one",
+      "$five$one$one",
+      "$five$one$one$one",
+      "$one$ten"
+    ));
+  }
+  return $table;
 }
 ?>
