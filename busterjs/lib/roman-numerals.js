6527e2d16bb92080280cf2abed9ae3e38c027ab5
Hugo Josefson <hugo.josefson@jayway.com>
2013-03-20 12:49:34 +0100

    Support for 4.

--- busterjs/lib/roman-numerals.js
+++ busterjs/lib/roman-numerals.js
@@ -1,27 +1,42 @@
+function literalValue(romanLiteral) {
+    if (romanLiteral === "") {
+        return 0;
+    } else if (romanLiteral === "I") {
+        return 1;
+    } else if (romanLiteral === "V") {
+        return 5;
+    } else if (romanLiteral === "X") {
+        return 10;
+    } else if (romanLiteral === "L") {
+        return 50;
+    } else if (romanLiteral === "C") {
+        return 100;
+    } else if (romanLiteral === "D") {
+        return 500;
+    } else if (romanLiteral === "M") {
+        return 1000;
+    } else {
+        throw new Error("Unsupported Roman literal: " + romanLiteral);
+    }
+
+}
+
 function romanToDecimal(romanInput) {
 
     var result = 0;
     var array = romanInput.split("");
     for (var i = 0; i < array.length; i++) {
         var romanLiteral = array[i];
-        if (romanLiteral === "") {
-            // 0
-        } else if (romanLiteral === "I") {
-            result += (1);
-        } else if (romanLiteral === "V") {
-            result += (5);
-        } else if (romanLiteral === "X") {
-            result += (10);
-        } else if (romanLiteral === "L") {
-            result += (50);
-        } else if (romanLiteral === "C") {
-            result += (100);
-        } else if (romanLiteral === "D") {
-            result += (500);
-        } else if (romanLiteral === "M") {
-            result += (1000);
+        var currentLiteralValue = literalValue(romanLiteral);
+        var nextIndex = i + 1;
+        var nextLiteral = nextIndex < array.length ? array[nextIndex] : "";
+        var nextLiteralValue = literalValue(nextLiteral);
+        if (nextLiteralValue > currentLiteralValue) {
+            var currentAndNextCalculatedValue = nextLiteralValue - currentLiteralValue;
+            result += currentAndNextCalculatedValue;
+            i++;
         } else {
-            throw new Error("Unsupported Roman literal at pos " + i + ": " + romanInput);
+            result += currentLiteralValue;
         }
     }
 
